cbca1d28fb0b73813b58fc438fe5fbbab00ecae5
siladitya <siladitya.dey@gmail.com>
2014-12-19 12:44:44 -0800

    Added variance per part for all columns

--- pml.proj.R
+++ pml.proj.R
@@ -4,7 +4,7 @@ pml.test<-as.data.table(read.csv('~/Downloads/pml-testing.csv'))
 #str(pml.train)
 #str(pml.test)
 
-dt.by.part<-function(dt.in, part.name) {
+get.dt.by.part<-function(dt.in, part.name) {
   dt.out<-dt.in[,grep(paste0('_',part.name,'$'), names(dt.in), value=T), with=F]
   for (c in names(dt.out)) {
     if ( !grepl('numeric', class(dt.out[[c]])) & !grepl('integer', class(dt.out[[c]])) ) {
@@ -20,22 +20,32 @@ dt.by.part<-function(dt.in, part.name) {
 #   dt.in - cbind(rep(col.means, nrow(dt.in)))
 # }
 
-dt.part.means<-function(col.means, dt.in) {
+get.dt.part.means<-function(col.means, dt.in) {
   dt.out<-dt.in[0]
   cat(dim(dt.out),'\n')
   cat(names(dt.out),'\n')
   for (c in names(col.means)) { # START: problem here
-    dt.out[[c]]<-rep(col.means[[c]], nrow(dt.in))
+    dt.out[[c]]<-c(rep(col.means[[c]], nrow(dt.in)))
   } # STOP: Problem here
   return(dt.out)
 }
 
+get.l.part.var<-function(dt.in) {
+  l.out<-list()
+  for (c in colnames(dt.in)) {
+    l.out[[c]]<-var(dt.in[[c]], na.rm=T)
+  }
+  return(l.out)
+}
+
 l_parts<-c('arm', 'belt', 'dumbbell', 'forearm')
 for (p in l_parts) {
-  dt.part<-dt.by.part(dt.in=pml.train, part.name=p)
+  dt.part<-get.dt.by.part(dt.in=pml.train, part.name=p)
   #cat(colMeans(dt.part, na.rm=T),'\n')
-  dt.means<-dt.part.means(colMeans(dt.part, na.rm=T), dt.in=dt.part)
-  dt.part.out<-dt.part-dt.means
-  dt.part.out[is.na(dt.part.out)]<-0
-  dt.part.out
+  dt.means<-get.dt.part.means(colMeans(dt.part, na.rm=T), dt.in=dt.part)
+  l.vars<-get.l.part.var(dt.part)
+  #str(l.vars)
+  #dt.part.out<-dt.part-dt.means
+  #dt.part.out[is.na(dt.part.out)]<-0
+  #dt.part.out
 }
