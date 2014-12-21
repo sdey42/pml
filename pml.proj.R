04ab7d11d0127fb9d6e1e5740bc8dafcd9e8354c
sila <siladitya.dey@gmail.com>
2014-12-21 12:37:26 -0800

    Done with final randomForest implementation - results correct!

--- pml.proj.R
+++ pml.proj.R
@@ -33,21 +33,24 @@ get.l.part.var<-function(dt.in) {
 get.dt.part.clean<-function(dt.in) {
   for (c in colnames(dt.in)) {
     if ( length(which(is.na(dt.in[[c]]))) > length(which(!is.na(dt.in[[c]]))) ) {
-      # Do something to remove that column
       dt.in[[c]]<-NULL
     }
   }
   return(dt.in)
 }
 
-get.dt.clean<-function(l.parts=c('arm', 'belt', 'dumbbell', 'forearm'), l.not.parts=c('X', 'user_name', 'new_window', 'num_window'), dt.in, is.train=F) {
+get.dt.clean<-function(l.parts=c('arm', 'belt', 'dumbbell', 'forearm'), l.not.parts=c('user_name', 'new_window', 'num_window'), dt.in, is.train=F, to.normalize=F) {
   dt.out<-data.table()
   for (p in l.parts) {
     dt.part<-get.dt.by.part(dt.in, part.name=p)
     dt.means<-get.dt.part.means(colMeans(dt.part, na.rm=T), dt.in=dt.part)
     l.vars<-get.l.part.var(dt.part)
-    dt.part.norm<-(dt.part - dt.means)/l.vars
-    dt.part.clean<-get.dt.part.clean(dt.part.norm)
+    if (to.normalize) {
+      dt.part.final<-(dt.part - dt.means)/l.vars
+    } else {
+      dt.part.final<-dt.part
+    }
+    dt.part.clean<-get.dt.part.clean(dt.part.final)
     if (nrow(dt.out)==0) {
       dt.out<-dt.part.clean
     } else {
@@ -64,29 +67,23 @@ get.dt.clean<-function(l.parts=c('arm', 'belt', 'dumbbell', 'forearm'), l.not.pa
 dt.train<-get.dt.clean(dt.in=pml.train, is.train=T)
 dt.test<-get.dt.clean(dt.in=pml.test)
 
+set.seed(1475)
 in.train<-createDataPartition(y=dt.train$classe, p=0.6, list=F)
 training<-as.data.frame(dt.train)[in.train,]
-#training.2<-as.data.frame(dt.train)[in.train,-c(20,19,18,17)]
 testing<-as.data.frame(dt.train)[-in.train,]
 
-set.seed(1475)
-#train.pc<-preProcess(training[,-c(21, 20, 19, 18, 17)], method='pca', thresh=0.95)
-#train.pc<-preProcess(training.2[,-dim(training.2)[2]], method='pca', thresh=0.95)
-#pred.pc<-predict(train.pc, training.2[,-dim(training.2)[2]])
-
-#model.rf<-train(classe~., data=training, method='rf', prox=T)
 cat('START randomForest...')
 model.rf<-randomForest(training[,-dim(training)[2]], training[, dim(training)[2]], prox=T)
-#model.rf1<-train(classe~ ., data=training, method='rf', prox=T)
 plot(model.rf)
-cat('STOP randomForest\n')
+cat('STOP\n')
 
-cat('START predict...')
+cat('START predict on cross-validation data...')
 pred<-predict(model.rf, testing)
 testing$predRight<-pred==testing$classe
 table(pred, testing$classe)
-cat('STOP predict\n')
+cat('STOP\n')
 
+cat('START predict on test data...')
 dt.test[, new_window:=factor(new_window, levels=c('no', 'yes'))]
 pred.new<-predict(model.rf, dt.test)
-#model.boost<-train(classe~., method='ada', data=training, verbose=F)
+cat('STOP\n')
